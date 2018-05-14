1e5a823bb73e0ef0959967c9ee4274e8c0cd8a09
Randytia Akbar <randytia.akbar@gmail.com>
2018-05-14 18:21:12 +0700

    14mei

--- app/src/main/java/com/example/user/ngaliyanhebat/SnackActivity.java
+++ app/src/main/java/com/example/user/ngaliyanhebat/SnackActivity.java
@@ -2,15 +2,20 @@ package com.example.user.ngaliyanhebat;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.ngaliyanhebat.Fragment.FragmentTematik;
 
 public class SnackActivity extends AppCompatActivity {
 
+    FloatingActionButton fabSnackContact;
     TextView telpon;
 
     @Override
@@ -18,8 +23,8 @@ public class SnackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snack);
 
-        telpon = (TextView) findViewById(R.id.telponSnack);
-        telpon.setOnClickListener(new View.OnClickListener() {
+        fabSnackContact = (FloatingActionButton) findViewById(R.id.fab_snack_contact);
+        fabSnackContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "089697177144", null));
@@ -27,5 +32,4 @@ public class SnackActivity extends AppCompatActivity {
             }
         });
     }
-
 }
